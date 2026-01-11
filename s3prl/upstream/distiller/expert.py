b73eee7ff181ff3151d5c2b2ce10932ad533d5d3
huiwen <ganhuiwen97@gmail.com>
2026-01-11 16:36:05 +0800

    bug fix for load_pretrain error

--- s3prl/upstream/distiller/expert.py
+++ s3prl/upstream/distiller/expert.py
@@ -18,24 +18,27 @@ class UpstreamExpert(UpstreamBase):
     def __init__(self, ckpt, model_config=None, **kwargs):
         super().__init__(**kwargs)
 
+        # Base options that are always needed
+        options = {
+            "load_pretrain": "True",
+            "no_grad": "False",
+            "permute_input": "False",
+        }
+
+        # Override with custom model config if provided
         if model_config is not None:
             print(
                 "[UpstreamExpert] - Using upstream expert config file from:",
                 model_config,
             )
-            with open(model_config, "r") as file:
-                options = yaml.load(file, Loader=yaml.FullLoader)
+            # model_config is the path to config_model.yaml, not options
+            # Don't override options, just pass the config path
         else:
             print("[UpstreamExpert] - Using the default upstream expert config")
-            options = {
-                "load_pretrain": "True",
-                "no_grad": "False",
-                "permute_input": "False",
-            }
 
         options["ckpt_file"] = ckpt
 
-        self.model = PretrainedDistiller(options)
+        self.model = PretrainedDistiller(options, config=model_config)
 
     def get_downsample_rates(self, key: str) -> int:
         return 320
